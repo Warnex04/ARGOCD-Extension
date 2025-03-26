5f15326805c2d845b4c463e71f5d54123c8e238b
Mohammad Baker <mohammad.baker@munic.io>
2025-03-26 10:22:49 +0100

    Add placeholder info for test

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,21 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 
-
 export const Extension = (props: any) => {
-  const [events, setEvents] = useState([]);
-  const [duration, setDuration] = useState("1h");
 
   const loc = window.location;
   const { resource, application } = props;
   const application_name = application?.metadata?.name || "";
 
-  const updateDuration = (e: any, dur: string) => {
-    e.preventDefault();
-    setDuration(dur);
-  };
-  useEffect(() => {
-    let url = `/api/v1/applications/${application_name}/events?resourceUID=${resource.metadata.uid}&resourceNamespace=${resource.metadata.namespace}&resourceName=${resource.metadata.name}&duration=${duration}`;
-    if (resource.kind === "Application") {
-      url = `/api/v1/applications/${application_name}/events`;
-    }
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        setEvents(data?.items || []);
-      })
-      .catch(err => {
-        console.error("res.data", err);
-      });
-  }, [application_name, resource, duration]);
-
-  return
+  return(
+    // Placeholder text
+    <React.Fragment>
+      <h1>Health Status</h1>
+      <p>Application: {application_name}</p>
+      <p>Progressing Devices: {resource?.status?.progressingDevices}</p>
+      <p>Updated Devices: {resource?.status?.updatedDevices}</p>
+    </React.Fragment>
+  );
 };
 
 export const component = Extension;
@@ -39,9 +25,8 @@ export const component = Extension;
   window?.extensionsAPI?.registerResourceExtension(
     component,
     "*",
-    "Rollout",
-    "Metrics",
+    "Fleet",
+    "Health Status",
     { icon: "fa fa-chart-area" }
   );
-  window?.extensionsAPI?.registerResourceExtension(component, '*', 'Fleet', 'Health Status', { icon: "fa fa-chart-area" });
 })(window);
